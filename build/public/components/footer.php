c0a359c9059b9c8fd4cb03249b9eb5f303dd7b4e
Przemek Trepka <przemek.trepka@gmail.com>
2016-09-15 13:17:17 +0200

    Basic layout + responsive

--- build/public/components/footer.php
+++ build/public/components/footer.php
@@ -2,13 +2,15 @@
   <nav class="nav__footer-menu--public">
     <ul class="footer-menu--public--left">
       <li><a href="#">O nas</a></li>
-      <li><a href="#">Pytania i odpowiedz</a></li>
+      <li><a href="#">Pytania i odpowiedzi</a></li>
       <li><a href="#">Oferta dla firm</a></li>
-      <li><a href="#">Kontakt</a></li>
+      <li><a href="#">Influencerzy</a></li>
+      <li><a href="campaign.php">Kampanie</a></li>
     </ul>
     <ul class="footer-menu--public--right">
       <li><a href="regulamin_public.php">Regulamin i prywatność</a></li>
-      <li><p>© 2016 Rekomenduj.to. Wszelkie prawa zastrzeżone.</p></li>
+      <li><a href="#">Kontakt</a></li>
+      <li class="footer__copyright"><p>© 2016 Rekomenduj.to. Wszelkie prawa zastrzeżone.</p></li>
     </ul>
   </nav>
 </footer>
