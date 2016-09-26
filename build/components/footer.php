6c35a95c7e55118538c4c90016ab7d7d2b240ebb
Przemek Trepka <przemek.trepka@gmail.com>
2016-09-26 19:22:31 +0200

    Add footer to inside scope

--- build/components/footer.php
+++ build/components/footer.php
@@ -1,2 +1,16 @@
 <footer>
+  <nav class="layout__container nav__footer-menu--inside">
+    <ul class="footer-menu--inside--left">
+      <li><a href="#">O nas</a></li>
+      <li><a href="#">Pytania i odpowiedzi</a></li>
+      <li><a href="#">Oferta dla firm</a></li>
+      <li><a href="#">Influencerzy</a></li>
+      <li><a href="campaign.php">Kampanie</a></li>
+    </ul>
+    <ul class="footer-menu--inside--right">
+      <li><a href="regulamin_public.php">Regulamin i prywatność</a></li>
+      <li><a href="#">Kontakt</a></li>
+      <li class="footer__copyright"><p>© 2016 Rekomenduj.to. Wszelkie prawa zastrzeżone.</p></li>
+    </ul>
+  </nav>
 </footer>
