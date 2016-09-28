4033422d2728596ac901b638f38b5dd808622f5b
agnieszka <agnieszka.rychlicka@gmail.com>
2016-09-28 16:20:35 +0200

    add public polityka

---
+++ build/polityka_public.php
@@ -0,0 +1,173 @@
+<?php
+
+$lang = 'pl';
+
+$tagsBodyBasic = 'preload scope--outside device--mobile';
+
+$tagsNav = '';
+$tagsTemplate = '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $lang ?>">
+<head>
+  <?php include 'public/components/head.php' ?>
+  <meta http-equiv="refresh">
+</head>
+
+<body class="<?php echo $tagsBodyBasic . ' ' . $tagsNav . ' ' . $tagsTemplate ?>">
+  <?php include 'public/components/bof-scripts.php' ?>
+
+  <div class="page__wrapper">
+
+    <?php include 'public/components/nav.php' ?>
+
+    <main>
+
+    <section class="layout--base paint--brand layout__header--content-page">
+      <h1>Polityka prywatności serwisu Rekomenduj.to</h1>
+    </section>
+    <section class="layout--base content--small-type content--privacy-policy">
+      <p>
+        Dziękujemy, że odwiedziłeś nasz serwis. Poniżej znajdziesz najważniejsze pytania i odpowiedzi dotyczące Twoich danych osobowych. Zapoznaj się z nimi szczegółowo, a w przypadku dodatkowych pytań skontaktuj się z nami. Dokładamy wszelkich starań, aby dane osobowe, które nam przekazujesz, były bezpieczne.
+      </p>
+      <h2>Jakie dane są zbierane od użytkowników?</h2>
+        <ol>
+            <li>Gromadzimy wyłącznie dane osobowe, które podajesz nam podczas rejestracji oraz otrzymane wskutek połączenia kont Rekomenduj.to z kontami w profilach społecznościowych, o którym mowa w punkcie II ust. 16 Regulaminu serwisu Rekomenduj.t.  Podczas rejestracji zbieramy bezpośrednie informacje osobiste, takie jak imię, nazwisko, adres i adres e-mail, telefon, a także dane pośrednie (poprzez Cookies, połączenie i informacje systemowe).</li>
+            <li>Dodatkowo w swoim profilu po dokonaniu rejestracji będziesz poproszony o podanie nam dodatkowych informacji o sobie, swoich zainteresowaniach itp. Po uzyskaniu od Ciebie pisemnej zgody na przetwarzanie danych osobowych, możesz też zostać poproszony o podanie danych ujawniających Twoje pochodzenie etniczne, dotyczących stanu zdrowia, nałogów lub życia seksualnego, takie jak: czy posiadasz uczulenie, czy jesteś osobą palącą etc. Wszystkie informacje są podawane przez Ciebie dobrowolnie, ale dzięki temu jesteśmy w stanie zaprosić Cię do naszych kampanii. Dodatkowo dane te są potrzebne, abyśmy mogli udostępnić Ci informacje na temat naszych akcji, ankiet, promocji i konkursów.</li>
+        </ol>
+      <h2>Kto jest administratorem danych osobowych?</h2>
+          <ol>
+            <li>Administratorem Twoich danych osobowych jest Buzz Media z siedzibą w Warszawie przy Al. Wilanowskiej 81/24 (kod pocztowy 02-765), NIP: 586 21 35 290. Rekomenduj.to jest serwisem internetowym prowadzonym przez Buzz Media pod domeną www.rekomenduj.to</li>
+          </ol>
+      <h2>Czym są cookies i dlaczego je zbieracie?</h2>
+          <ol>
+            <li>Za pomocą technologii cookie zbieramy jedynie anonimowe dane statystyczne o użytkownikach służące do poprawy wygody korzystania z serwisu. Dla zwiększenia wydajności stosujemy technologię anonimowych cookie oraz pobieramy dane statystyczne o najczęściej poszukiwanych za pomocą wyszukiwarki frazach i dzięki temu aktualizujemy na bieżąco nawigację serwisu, aby wyszukiwanie treści było wygodne i intuicyjne. Czym są cookies? Są to małe pliki tekstowe pochodzące z witryny www zapamiętywane przez przeglądarkę internetową użytkownika serwisu. Samo wysłanie cookie do przeglądarki w żaden sposób nie narusza prywatności użytkownika. Informacje zawarte w cookie wykorzystywane mogą być jedynie przez stronę internetową, z której zostały wysłane.</li>
+          </ol>
+      <h2>W jaki sposób moje dane są wykorzystywane, a w szczególności czy są przekazywane innym firmom?</h2>
+          <ol>
+            <li>Twoje dane osobowe będą wykorzystywane wyłącznie do celów, dla których udostępniłeś je Buzz Media bezpośrednio lub poprzez połączenie kont, o którym mowa w punkcie II ust. 16 Regulaminu serwisu Rekomenduj.to. Cele te są wskazane w Regulaminie serwisu Rekomenduj.to. Twoje dane osobowe będą również wykorzystywane w celu zarządzania, obsługi i uzyskiwania informacji zwrotnych na temat poziomu naszych usług, a także zapobiegania naruszeniom zabezpieczeń, prawa i warunków umownych.</li>
+            <li>
+              Możesz być spokojny – nie udostępnimy Twoich danych innych firmom bez Twojej wyraźnej zgody.
+            </li>
+            <li>
+              Pamiętaj, że w przypadku gdy zostaniemy do tego zobowiązani na mocy obowiązujących przepisów albo jeżeli w naszej ocenie udostępnienie danych jest w uzasadniony sposób konieczne, możemy także przekazywać Twoje dane osobowe instytucjom rządowym oraz organom ścigania.
+            </li>
+            <li>
+              W przypadku Twojego udziału w kampaniach organizowanych dla naszych Klientów i wysyłce do Ciebie paczki  z produktami do testowania, Twój adres do wysyłki, telefon kontaktowy oraz adres e-mail zostanie przekazany firmie kurierskiej. Ponadto Twój telefon kontaktowy oraz adres e-mail, może zostać przekazany firmie badawczej w celu przeprowadzenia ankiety dotyczącej testowanego produktu.
+            </li>
+          </ol>
+      <h2>W jaki sposób będziecie się ze mną kontaktować?</h2>
+      <ol>
+        <li>
+          Kontaktować się z Tobą będziemy przede wszystkim drogą elektroniczną na wskazany przez Ciebie w formularzu rejestracyjnym adres e-mail. Dodatkowo możemy kontaktować się z Tobą telefonicznie (rozmowa lub wiadomość sms).
+        </li>
+        <li>
+          W przypadku Twojego udziału w kampanii organizowanej dla naszych Klientów, wykorzystamy również Twój adres domowy w celu wysłania do Ciebie przesyłki.
+        </li>
+      </ol>
+      <h2>W jaki sposób mogę dokonać zmian w moich danych osobowych lub usunąć swoje konto?</h2>
+      <ol>
+        <li>
+          Swoje dane osobowe możesz zmienić w swoim profilu po zalogowaniu się w serwisie rekomenduj.to. W celu usunięcia swojego konta w serwisie Rekomenduj.to, możesz do nas napisać na adres rezygnuje@rekomenduj.to, w temacie maila wpisując „Usunięcie konta”. W przypadku problemów zawsze możesz skontaktować się z nami.
+        </li>
+      </ol>
+      <h2>W jaki sposób są zabezpieczane moje dane?</h2>
+      <ol>
+        <li>
+          W celu zapewnienia bezpieczeństwa Twoich danych osobowych Buzz Media utrzymuje bezpieczne środowisko informatyczne i posiada stosowne środki zapobiegania nieautoryzowanemu dostępowi. Dostęp do Twoich danych osobowych mają wyłącznie upoważnieni pracownicy Buzz Media.
+        </li>
+      </ol>
+      <h2>Jak długo przechowujemy Twoje dane osobowe?</h2>
+      <ol>
+        <li>
+          W przypadku  gdy wyraziłeś zgodę na przetwarzanie danych osobowych przez Buzz Media w celach marketingowych, będziemy przechowywać Twoje dane bezterminowo, do czasu likwidacji bazy danych, w których Twoje dane są przechowywane lub do czasu otrzymania od Ciebie prośby o ich usunięcie.  W pozostałych przypadkach będziemy przechowywać Twoje dane osobowe jedynie przez okres niezbędny z punktu widzenia naszych potrzeb.
+        </li>
+      </ol>
+      <h2>Polityka Cookies</h2>
+      <ol>
+        <li>
+          Opisana Polityka Plików Cookies, dotycząca tzw. ciasteczek odnosi się do wszystkich stron internetowych należących do serwisu www.rekomenduj.to (Strony Rekomenduj.to). Poprzez używanie Strony Rekomenduj.to wyrażasz zgodę na używanie ciasteczek zgodnie z niniejszą Polityką Ciasteczek. Jeżeli nie zgadzasz się na używanie ciasteczek na Stronach Rekomenduj.to, zmień ustawienia swojej przeglądarki lub zrezygnuj z używania Stron Rekomenduj.to. Wyłączenie stosowania cookies może spowodować utrudnienia w korzystania z niektórych usług w ramach Stron Rekomenduj.to, w szczególności wymagających logowania. Wyłączenie opcji przyjmowania cookies może powodować brak możliwości czytania lub oglądania treści zamieszczanych.
+        </li>
+        <li>
+          <strong>Co to są ciasteczka - cookies ? </strong>
+
+            Informacje zapisywane w postaci plików przez serwery na urządzeniu końcowym użytkownika, które serwery mogą odczytać przy każdorazowym połączeniu się z tego urządzenia końcowego, może także używać innych technologii o funkcjach podobnych lub tożsamych z cookies.
+            Więcej informacji znajdziesz <a href="https://pl.wikipedia.org/wiki/HTTP_cookie" target="_blank">tu</a>.
+        </li>
+        <li>
+            Jak wyłączyć pliki cookie w przeglądarce?
+        </li>
+        <li>
+            <ol>
+              <li>
+                <a href="http://help.opera.com/Windows/12.10/pl/cookies.html" target="_blank">Przeglądarka Opera</a>
+              </li>
+              <li>
+                <a href="https://support.mozilla.org/pl/kb/ciasteczka" target="_blank">Przeglądarka Firefox</a>
+              </li>
+              <li>
+                <a href="https://support.google.com/chrome/answer/95647?hl=pl" target="_blank">Przeglądarka Chrome</a>
+              </li>
+              <li>
+                <a href="https://support.microsoft.com/pl-pl/kb/278835" target="_blank">Przeglądarka Internet Explorer</a>
+              </li>
+              <li>
+                <a href="https://support.apple.com/pl-pl/HT201265" target="_blank">Przeglądarka Safari</a>
+              </li>
+            </ol>
+        </li>
+        <li>
+          W ramach Stron Rekomenduj.to stosowane są następujące rodzaje plików cookies:
+        </li>
+        <li>
+          <ol>
+            <li>
+              <strong>sesyjne</strong> - stanowią pliki tymczasowe, przechowywane w urządzeniu końcowym użytkownika. Po wylogowaniu, opuszczenia Strony Rekomenduj.to lub wyłączeniu oprogramowania (przeglądarki internetowej) są usuwane. Służą do obsługi usług wymagających uwierzytelniania w ramach danej Strony.
+            </li>
+            <li>
+              <strong>stałe</strong> - przechowywane są w urządzeniu końcowym Użytkownika przez czas określony w parametrach plików cookies lub do czasu ich usunięcia przez użytkownika i służą do zapamiętania wybranych przez użytkownika Serwisu ustawień i personalizacji interfejsu użytkownika.
+            </li>
+            <li>
+              <strong>zewnętrzne</strong> - umożliwiają zbieranie informacji o sposobie korzystania z Stron Rekomenduj.to oraz dostarczanie użytkownikom treści reklamowych, dostosowanych do zainteresowań poszczególnych użytkowników.
+            </li>
+          </ol>
+          <li>
+            <strong>Ciasteczka Google Analytics</strong><br />
+            Strony Rekomenduj.to korzystają z mechanizmu Google Analytics w celu zbierania statystyk dla Stron Rekomenduj.to. Więcej informacji znajdziesz na stronach: <a href="https://developers.google.com/analytics/devguides/collection/gajs/cookie-usage" target="_blank">Google Analytics Cookie Usage on Websites (EN)</a> oraz
+            <a href="https://www.google.com/intl/pl/policies/privacy/" target="_blank">Polityka Prywatności Google (PL)</a>.
+          </li>
+        </li>
+      </li>
+    </ol>
+      <h2>Zmiany</h2>
+      <ol>
+        <li>
+          Usługi i funkcje w ramach serwisu będą z czasem ulegać rozszerzeniu. Oznacza to m.in., że w przyszłości możemy wprowadzić zmiany w niniejszym dokumencie. Wraz z każdą zmianą nowa wersja Polityki Prywatności będzie się pojawiać z nową datą. W takim przypadku poprosimy Cię o potwierdzenie zapoznania się z nową wersją Polityki Prywatności i jej akceptacji.
+        </li>
+        <li>
+          Warszawa, wrzesień 2016 r.
+        </li>
+      </ol>
+        </section>
+    </main>
+
+    <?php include 'public/components/footer.php' ?>
+  </div>
+
+  <?php
+  include 'components/eof-scripts.php'
+  ?>
+  <!-- Initialize Swiper -->
+  <script>
+  var swiper = new Swiper('.cs__container', {
+    pagination: '.cs__pagination',
+      slidesPerView: 1.1,
+      centeredSlides: true,
+      paginationClickable: true,
+      spaceBetween: 10
+    });
+  </script>
+</body>
+</html>
